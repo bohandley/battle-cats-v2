039460563d151b97370b07b3bd9bca7eb21b8479
Brendan O'Handley <brendan.ohandley@gmail.com>
2019-02-10 17:53:38 -0500

    remove es6 syntax

--- app/assets/javascripts/userTabs.js
+++ app/assets/javascripts/userTabs.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 function tabSwitcher(){
     $(".nav-tabs a").on("click", function(e){
         e.preventDefault();
-        let $this = $(this);
-        
+        var $this = $(this);
+
         // remove active class from tabs and viewer
         $(".nav-tabs li.active").removeClass("active");
         $(".tab-pane.active").removeClass("active");
 
         // add active class to li and container
-        let id = $this.attr("href");
+        var id = $this.attr("href");
         $this.parent().addClass("active");
         $(".tab-pane"+id).addClass("active");
     });
