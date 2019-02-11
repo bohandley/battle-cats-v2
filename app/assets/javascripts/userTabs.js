0ec60f2ae6ca91c4260b92a76d9b5014e9d9b1d5
Brendan O'Handley <brendan.ohandley@gmail.com>
2019-02-10 20:18:28 -0500

    remove unneccessary scripts in application

--- app/assets/javascripts/userTabs.js
+++ app/assets/javascripts/userTabs.js
@@ -5,12 +5,15 @@ $(document).ready(function() {
 function tabSwitcher(){
     $(".nav-pills a").on("click", function(e){
         e.preventDefault();
+        var $this = $(this);
 
+        // remove active class from tabs and viewer
         $(".nav-pills li.active").removeClass("active");
-        
-        var $this   = $(this),
-            id      = $this.attr("href");
+        $(".tab-pane.active").removeClass("active");
 
+        // add active class to li and container
+        var id = $this.attr("href");
         $this.parent().addClass("active");
+        $(".tab-pane"+id).addClass("active");
     });
 }
